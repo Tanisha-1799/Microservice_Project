b44d55cee6fb622b688841542f5bb6788a2c0328
Tanisha-1799 <triveditanisha01171999@gmail.com>
2024-02-26 19:14:25 +0530

    Adding the sql table configurations to the project

--- order-service/order-service/src/main/java/com/tantech/orderservice/repository/OrderRepository.java
+++ order-service/order-service/src/main/java/com/tantech/orderservice/repository/OrderRepository.java
@@ -4,4 +4,5 @@ import com.tantech.orderservice.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    //An interface that follows a particular structure for the sql table
 }
