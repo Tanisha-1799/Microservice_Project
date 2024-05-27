b77e8824467c1f2ecf5e3ee75218d0c63f561458
Tanisha-1799 <triveditanisha01171999@gmail.com>
2024-05-27 22:01:27 +0530

    adding necessary comments to the project

--- order-service/order-service/src/main/java/com/tantech/orderservice/repository/OrderRepository.java
+++ order-service/order-service/src/main/java/com/tantech/orderservice/repository/OrderRepository.java
@@ -4,5 +4,5 @@ import com.tantech.orderservice.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+    //A repository interface that extends JPA so that we can get a model for creating a database
 }
